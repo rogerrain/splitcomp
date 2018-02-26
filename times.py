b7d8295aa22ef74fe7e4622a56f7607075d781c0
Roger <tanimowr@mcmaster.ca>
2018-02-26 17:02:20 -0500

    Base comparer complete, no error checking yet

--- times.py
+++ times.py
@@ -7,19 +7,39 @@ def getTime():
     return ss
 
 def toMS(time):
-    assert len(time) <= 5
-    conversions = [1, 1000, 60000, 3600000, 86400000]
+    assert len(time) <= 4
+    time.reverse() #Lining up the time segments with the conversion order
+    conversions = [1, 1000, 60000, 3600000]
     ms = 0
     for i in range(0, len(time)):
-        ms += time[i]*conversions[i]
+        ms += int(time[i])*conversions[i]
     return ms
+
+def fromMS(ms):
+    conversions = [3600000, 60000, 1000, 1]
+    t = []
+    for i in range(0, len(conversions)):
+        current = ms // conversions[i]
+        ms -= current * conversions[i]
+        t.append(current)
+    timeString = "{}:{}:{}.{}".format(t[0], t[1], t[2], t[3])
+    return timeString
 
 def main():
     timeList = getTime()
     for time in timeList:
         print(time)
+    ms = toMS(timeList)
+    print(ms)
+    timeList2 = getTime()
+    for time in timeList2:
+        print(time)
+    ms2 = toMS(timeList2)
+    print(ms2)
+    total = abs(ms - ms2)
+    print(total)
+    time = fromMS(total)
+    print(time)
 
-'''
 if __name__ == "__main__":
     main()
-'''
